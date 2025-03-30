f2f187ed79ee19f734cefe4245861890aca2a0ec
John Joseph <jpothen8@gmail.com>
2025-03-30 14:54:41 -0400

    make ktlint happy

--- src/main/kotlin/frc/team449/subsystems/superstructure/elevator/Elevator.kt
+++ src/main/kotlin/frc/team449/subsystems/superstructure/elevator/Elevator.kt
@@ -17,7 +17,6 @@ import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand
 import frc.team449.subsystems.superstructure.SuperstructureGoal
-import frc.team449.subsystems.superstructure.pivot.PivotConstants
 import frc.team449.subsystems.superstructure.wrist.WristConstants
 import frc.team449.system.motor.KrakenDogLog
 import java.util.function.Supplier
