809c4b39c44156287caca3933b5e8cc28013d344
John Joseph <jpothen8@gmail.com>
2025-04-02 13:10:51 -0400

    make ktlint happy

--- src/main/kotlin/frc/team449/subsystems/FieldConstants.kt
+++ src/main/kotlin/frc/team449/subsystems/FieldConstants.kt
@@ -3,7 +3,6 @@ package frc.team449.subsystems
 import edu.wpi.first.math.MathUtil
 import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Rotation2d
-import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.wpilibj.DriverStation.Alliance
 import kotlin.math.PI
 
