007b2996da98a3d02fae944affa9f00e6534d58f
Tinbite-A <tinbite724@gmail.com>
2025-06-05 18:18:28 -0400

    set climb id

--- src/main/kotlin/frc/team449/subsystems/superstructure/climb/ClimbConstants.kt
+++ src/main/kotlin/frc/team449/subsystems/superstructure/climb/ClimbConstants.kt
@@ -3,7 +3,7 @@ package frc.team449.subsystems.superstructure.climb
 import edu.wpi.first.units.Units.Amps
 
 object ClimbConstants {
-  const val MOTOR_ID = 62
+  const val MOTOR_ID = 49
 
   val CURRENT_LIMIT = Amps.of(60.0)
   const val BRAKE_MODE = true
