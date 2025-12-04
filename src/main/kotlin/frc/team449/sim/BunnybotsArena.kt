6eeab7830b597dad7a3591b8dcf32a2875c20f02
Michael Spears <mts18142027@gmail.com>
2025-12-04 17:17:10 -0500

    Updated Bunnybots Arena Simulation

--- src/main/kotlin/frc/team449/sim/BunnybotsArena.kt
+++ src/main/kotlin/frc/team449/sim/BunnybotsArena.kt
@@ -14,7 +14,7 @@ class BunnybotsArena() : SimulatedArena(BunnybotsFieldMap()) {
       // Coordinates are relative to bottom left of field, with blue on the left
 
       // AdvantageScope Model Offset
-      val offset = Translation2d(0.673100/2+0.088900, -0.4318/2)
+      val offset = Translation2d(0.673100/2+0.088900, -0.431800/2+0.088900)
 
       // Bottom Wall
       super.addBorderLine(
@@ -81,9 +81,111 @@ class BunnybotsArena() : SimulatedArena(BunnybotsFieldMap()) {
         Translation2d(16.548100, 6.184900).plus(offset)
       )
 
+      // Star Spires
+
+      // Left
+      super.addBorderLine(
+        Translation2d(0.088900, 7.251700).plus(offset),
+        Translation2d(0.190500, 7.251700).plus(offset)
+      )
+      super.addBorderLine(
+        Translation2d(0.190500, 7.251700).plus(offset),
+        Translation2d(0.190500, 6.642100).plus(offset)
+      )
+      super.addBorderLine(
+        Translation2d(0.190500, 6.642100).plus(offset),
+        Translation2d(0.088900, 6.642100).plus(offset)
+      )
+
+      // Top Left
+      super.addBorderLine(
+        Translation2d(1.612900, 8.318500).plus(offset),
+        Translation2d(1.612900, 8.216900).plus(offset)
+      )
+      super.addBorderLine(
+        Translation2d(1.612900, 8.216900).plus(offset),
+        Translation2d(2.222500, 8.216900).plus(offset)
+      )
+      super.addBorderLine(
+        Translation2d(2.222500, 8.216900).plus(offset),
+        Translation2d(2.222500, 8.318500).plus(offset)
+      )
+
+      // Top Right
+      super.addBorderLine(
+        Translation2d(14.414500, 8.318500).plus(offset),
+        Translation2d(14.414500, 8.216900).plus(offset)
+      )
+      super.addBorderLine(
+        Translation2d(14.414500, 8.216900).plus(offset),
+        Translation2d(15.024100, 8.216900).plus(offset)
+      )
+      super.addBorderLine(
+        Translation2d(15.024100, 8.216900).plus(offset),
+        Translation2d(15.024100, 8.318500).plus(offset)
+      )
+
+      // Right
+      super.addBorderLine(
+        Translation2d(16.548100, 7.251700).plus(offset),
+        Translation2d(16.446500, 7.251700).plus(offset)
+      )
+      super.addBorderLine(
+        Translation2d(16.446500, 7.251700).plus(offset),
+        Translation2d(16.446500, 6.642100).plus(offset)
+      )
+      super.addBorderLine(
+        Translation2d(16.446500, 6.642100).plus(offset),
+        Translation2d(16.548100, 6.642100).plus(offset)
+      )
+
+      // Cosmic Converters
+
+      // Bottom Left
+      super.addBorderLine(
+        Translation2d(0.088900, 1.130300).plus(offset),
+        Translation2d(0.952500, 1.130300).plus(offset)
+      )
+      super.addBorderLine(
+        Translation2d(0.952500, 1.130300).plus(offset),
+        Translation2d(0.952500, 0.088900).plus(offset)
+      )
+
+      // Bottom Right
+      super.addBorderLine(
+        Translation2d(16.548100, 1.130300).plus(offset),
+        Translation2d(15.684500, 1.130300).plus(offset)
+      )
+      super.addBorderLine(
+        Translation2d(15.684500, 1.130300).plus(offset),
+        Translation2d(15.684500, 0.088900).plus(offset)
+      )
+
+      // Top Left
+      super.addBorderLine(
+        Translation2d(0.088900, 4.549775).plus(offset),
+        Translation2d(0.952500, 4.549775).plus(offset)
+      )
+      super.addBorderLine(
+        Translation2d(0.952500, 4.549775).plus(offset),
+        Translation2d(0.952500, 5.591175).plus(offset)
+      )
+
+      // Top Right
+      super.addBorderLine(
+        Translation2d(16.548100, 4.530725).plus(offset),
+        Translation2d(15.684500, 4.530725).plus(offset)
+      )
+      super.addBorderLine(
+        Translation2d(15.684500, 4.530725).plus(offset),
+        Translation2d(15.684500, 5.572125).plus(offset)
+      )
+
+      // Field Obstacles
+
       // Center Box [Bottom Left Corner: (7.883525, 3.684588), Top Right Corner: (8.753475, 4.722813), Center: (8.3185, 4.2037005) ]
-      super.addRectangularObstacle(1.038225, 0.869950, Pose2d(
-        Translation2d(8.3185, 4.2037005).plus(offset),
+      super.addRectangularObstacle(0.869950, 1.038225, Pose2d(
+        Translation2d(8.318500, 4.203700).plus(offset),
         Rotation2d()
       ) )
 
@@ -98,12 +200,12 @@ class BunnybotsArena() : SimulatedArena(BunnybotsFieldMap()) {
         Rotation2d(Degrees.of(-44.4855955))
       ) )
       // Bottom Left Box
-      super.addRectangularObstacle(0.609600,0.869950,  Pose2d(
+      super.addRectangularObstacle(0.869950, 0.609600,Pose2d(
         Translation2d(6.474907, 2.237885).plus(offset),
         Rotation2d(Degrees.of(-44.4855955))
       ) )
       // Bottom Right Box
-      super.addRectangularObstacle(0.609600, 0.869950, Pose2d(
+      super.addRectangularObstacle(0.869950, 0.609600, Pose2d(
         Translation2d(10.162093, 2.237885).plus(offset),
         Rotation2d(Degrees.of(44.4855955))
       ) )
